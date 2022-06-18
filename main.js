34aa5115df07f61ebad85d9b6e68608a428e97ce
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-06-18 14:35:43 -0700

    completed meme gallery project

---
+++ main.js
@@ -0,0 +1,29 @@
+var $gallery = document.querySelector('main');
+
+var imageUrls = [];
+
+var $imageUrlInput = document.querySelector('input');
+
+var $addImageButton = document.querySelector('button');
+
+$addImageButton.addEventListener('click', function () {
+
+  if ($imageUrlInput.value !== '') {
+    imageUrls.push($imageUrlInput.value)
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
+});
+
+function updateGallery() {
+  $gallery.innerHTML = '';
+
+  for (var i = 0; i < imageUrls.length; i++) {
+    var $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+
+updateGallery();
